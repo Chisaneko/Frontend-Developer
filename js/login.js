1a5def308339eb28f995694f59acaa5061e76525
Chisaneko <david4sayt@gmail.com>
2023-08-15 12:29:02 -0300

    Autentificação e JSON

--- js/login.js
+++ js/login.js
@@ -6,12 +6,54 @@ const emailBanco = "admin@admin"
 const senhaBanco = "123"
 
 btnEntrar.addEventListener('click', () => {
-    let temail = email.value
-    let tsenha = senha.value
+    let userEmail = email.value
+    let userSenha = senha.value
 
-    if (temail == emailBanco && tsenha ==senhaBanco){
-        window.open('cadastro-usuário.html', '_self')
-    } else {
-        window.alert('Senha incorreta')
+    if (userEmail == "" || userSenha == ""){
+        window.alert('O campo de e-mail e senha são obrigatórios')
+        return
     }
+
+    autenticar(userEmail, userSenha)
+    
+    /*if (userEmail != emailBanco || userSenha != senhaBanco){
+        window.alert('Email ou senha incorreto')
+        return
+    }
+
+    if (userEmail == emailBanco && userSenha == senhaBanco){
+        window.open('cadastro-usuário.html', '_self')
+    } 
+    */
 })
+
+function autenticar(email, senha){
+    const urlBase = 'http://localhost:3400'
+    fetch(`${urlBase}/login`,{
+        method:'POST',
+        headers:{
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({email, senha})
+        })
+        .then(response => response = response.json())
+        .then(response => {
+            if(!!response.mensagem){
+                alert (response.mensagem)
+                return
+            } else{
+                alert ("Usuário autenticado com sucesso!")
+                salvarToken(response.token)
+                salvarUsuario(response.usuario)
+                window.open('cadastro-usuário.html', '_self')
+            }
+        })
+}
+
+function salvarToken (token){
+    localStorage.setItem('token', token)
+}
+
+function salvarUsuario (usuario){
+    localStorage.setItem('usuario', JSON.stringify(usuario))
+}
